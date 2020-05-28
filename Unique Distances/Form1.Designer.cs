56ae295de52b032140c81480d229c4e23f27a838
Ithilrist <53412289+Ithilrist@users.noreply.github.com>
2020-05-27 19:28:51 -0700

    Got tired of trying to make things nicely, went with brute force.
    I had something going with my spreadsheets, starting to see a pattern, but couldn't find the logical step to go from 3 to 4, so couldn't take it from there. Decided that brute force and getting ALL the solutions (including reflections and rotations because I'm too lazy to do the math). Yes there are efficiencies that could be added, for example not having points double check themselves, but again, this was frustration coding.

--- Unique Distances/Form1.Designer.cs
+++ Unique Distances/Form1.Designer.cs
@@ -28,13 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.P1X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P1Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P2X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P2Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P3X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P3Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P4X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P4Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P5X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P5Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P6X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P6Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.P1X,
+            this.P1Y,
+            this.P2X,
+            this.P2Y,
+            this.P3X,
+            this.P3Y,
+            this.P4X,
+            this.P4Y,
+            this.P5X,
+            this.P5Y,
+            this.P6X,
+            this.P6Y});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1261, 395);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // P1X
+            // 
+            this.P1X.HeaderText = "P1X";
+            this.P1X.Name = "P1X";
+            this.P1X.ReadOnly = true;
+            // 
+            // P1Y
+            // 
+            this.P1Y.HeaderText = "P1Y";
+            this.P1Y.Name = "P1Y";
+            this.P1Y.ReadOnly = true;
+            // 
+            // P2X
+            // 
+            this.P2X.HeaderText = "P2X";
+            this.P2X.Name = "P2X";
+            this.P2X.ReadOnly = true;
+            // 
+            // P2Y
+            // 
+            this.P2Y.HeaderText = "P2Y";
+            this.P2Y.Name = "P2Y";
+            this.P2Y.ReadOnly = true;
+            // 
+            // P3X
+            // 
+            this.P3X.HeaderText = "P3X";
+            this.P3X.Name = "P3X";
+            this.P3X.ReadOnly = true;
+            // 
+            // P3Y
+            // 
+            this.P3Y.HeaderText = "P3Y";
+            this.P3Y.Name = "P3Y";
+            this.P3Y.ReadOnly = true;
+            // 
+            // P4X
+            // 
+            this.P4X.HeaderText = "P4X";
+            this.P4X.Name = "P4X";
+            this.P4X.ReadOnly = true;
+            // 
+            // P4Y
+            // 
+            this.P4Y.HeaderText = "P4Y";
+            this.P4Y.Name = "P4Y";
+            this.P4Y.ReadOnly = true;
+            // 
+            // P5X
+            // 
+            this.P5X.HeaderText = "P5X";
+            this.P5X.Name = "P5X";
+            this.P5X.ReadOnly = true;
+            // 
+            // P5Y
+            // 
+            this.P5Y.HeaderText = "P5Y";
+            this.P5Y.Name = "P5Y";
+            this.P5Y.ReadOnly = true;
+            // 
+            // P6X
+            // 
+            this.P6X.HeaderText = "P6X";
+            this.P6X.Name = "P6X";
+            this.P6X.ReadOnly = true;
+            // 
+            // P6Y
+            // 
+            this.P6Y.HeaderText = "P6Y";
+            this.P6Y.Name = "P6Y";
+            this.P6Y.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1286, 420);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P1X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P1Y;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P2X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P2Y;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P3X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P3Y;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P4X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P4Y;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P5X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P5Y;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P6X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P6Y;
     }
 }
 
--- Unique Distances/Form1.cs
+++ Unique Distances/Form1.cs
@@ -2,19 +2,110 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+//using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows;
+
 
 namespace Unique_Distances
 {
     public partial class Form1 : Form
     {
+        static int _Size = 6;
+        Point[,] Points = new Point[_Size, _Size];
         public Form1()
         {
             InitializeComponent();
+            PopulatePoints();
+            DoWork();
+        }
+
+        void PopulatePoints()
+        {
+            for (int i = 0; i < _Size; i++)
+            {
+                for (int j = 0; j < _Size; j++)
+                {
+                    var p = new Point(i, j);
+                    Points[i, j] = p;
+                }
+            }
+        }
+
+        void DoWork()
+        {
+            foreach (var p1 in Points)
+            {
+                foreach (var p2 in Points)
+                {
+                    foreach (var p3 in Points)
+                    {
+                        foreach (var p4 in Points)
+                        {
+                            foreach (var p5 in Points)
+                            {
+                                foreach (var p6 in Points)
+                                {
+                                    var d12 = Point.Subtract(p2, p1).Length;
+                                    var d13 = Point.Subtract(p3, p1).Length;
+                                    if (d12 == d13)
+                                        continue;
+                                    var d14 = Point.Subtract(p4, p1).Length;
+                                    if (d12 == d14 || d13 == d14)
+                                        continue;
+                                    var d15 = Point.Subtract(p5, p1).Length;
+                                    if (d12 == d15 || d13 == d15 || d14 == d15)
+                                        continue;
+                                    var d16 = Point.Subtract(p6, p1).Length;
+                                    if (d12 == d16 || d13 == d16 || d14 == d16 || d15 == d16)
+                                        continue;
+
+                                    var d23 = Point.Subtract(p3, p2).Length;
+                                    if (d12 == d23 || d13 == d23 || d14 == d23 || d15 == d23 || d16 == d23)
+                                        continue;
+                                    var d24 = Point.Subtract(p4, p2).Length;
+                                    if (d12 == d24 || d13 == d24 || d14 == d24 || d15 == d24 || d16 == d24 || d23 == d24)
+                                        continue;
+                                    var d25 = Point.Subtract(p5, p2).Length;
+                                    if (d12 == d25 || d13 == d25 || d14 == d25 || d15 == d25 || d16 == d25 || d23 == d25 || d24 == d25)
+                                        continue;
+                                    var d26 = Point.Subtract(p6, p2).Length;
+                                    if (d12 == d26 || d13 == d26 || d14 == d26 || d15 == d26 || d16 == d26 || d23 == d26 || d24 == d26 || d25 == d26)
+                                        continue;
+
+
+                                    var d34 = Point.Subtract(p4, p3).Length;
+                                    if (d12 == d34 || d13 == d34 || d14 == d34 || d15 == d34 || d16 == d34 || d23 == d34 || d24 == d34 || d25 == d34 || d26 == d34)
+                                        continue;
+                                    var d35 = Point.Subtract(p5, p3).Length;
+                                    if (d12 == d35 || d13 == d35 || d14 == d35 || d15 == d35 || d16 == d35 || d23 == d35 || d24 == d35 || d25 == d35 || d26 == d35 || d34 == d35)
+                                        continue;
+                                    var d36 = Point.Subtract(p6, p3).Length;
+                                    if (d12 == d36 || d13 == d36 || d14 == d36 || d15 == d36 || d16 == d36 || d23 == d36 || d24 == d36 || d25 == d36 || d26 == d36 || d34 == d36 || d35 == d36)
+                                        continue;
+
+
+                                    var d45 = Point.Subtract(p5, p4).Length;
+                                    if (d12 == d45 || d13 == d45 || d14 == d45 || d15 == d45 || d16 == d45 || d23 == d45 || d24 == d45 || d25 == d45 || d26 == d45 || d34 == d45 || d35 == d45 || d36 == d45)
+                                        continue;
+                                    var d46 = Point.Subtract(p6, p4).Length;
+                                    if (d12 == d46 || d13 == d46 || d14 == d46 || d15 == d46 || d16 == d46 || d23 == d46 || d24 == d46 || d25 == d46 || d26 == d46 || d34 == d46 || d35 == d46 || d36 == d46 || d45 == d46)
+                                        continue;
+
+                                    var d56 = Point.Subtract(p6, p5).Length;
+                                    if (d12 == d56 || d13 == d56 || d14 == d56 || d15 == d56 || d16 == d56 || d23 == d56 || d24 == d56 || d25 == d56 || d26 == d56 || d34 == d56 || d35 == d56 || d36 == d56 || d45 == d56 || d46 == d56)
+                                        continue;
+
+                                    dataGridView1.Rows.Add(p1.X, p1.Y, p2.X, p2.Y,p3.X, p3.Y,p4.X, p4.Y,p5.X, p5.Y,p6.X, p6.Y);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
